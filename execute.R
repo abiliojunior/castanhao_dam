472c147d4d2139b0a1160fb03e560ae24a4aa261
abiliojunior <abiliojunior.mat@gmail.com>
2019-04-18 19:55:08 -0300

    first commit

---
+++ execute.R
@@ -0,0 +1,44 @@
+library(jsonlite)
+library(tidyverse)
+library(lubridate)
+library(gganimate)
+
+
+
+json_file_teste <- "./Data/teste.json"
+json_file <- "./Data/castanhao01.json"
+#json_file <- "./Data/resposta.json"
+
+json_data <- fromJSON(json_file, simplifyVector = FALSE)
+
+json_data_frame <- data.frame(matrix(unlist(json_data), nrow=length(json_data), byrow=T))
+
+names(json_data_frame)<-c("datamedicao", "timestamp","cota","volume","Volume_perc")
+
+as.numeric(str_replace(json_data_frame$cota, ",", "."))-> json_data_frame$cota
+as.numeric(str_replace(json_data_frame$volume, ",", "."))-> json_data_frame$volume
+as.numeric(str_replace(json_data_frame$Volume_perc, ",", "."))-> json_data_frame$Volume_perc
+json_data_frame<-json_data_frame[,-2]
+
+
+#######################################################################################
+
+df<-json_data_frame
+
+#tranformando em date time posic
+
+df$datamedicao<-as.POSIXct(df$datamedicao, format="%Y-%m-%d")
+df <- na.omit(df)
+
+lims <- as.POSIXct(strptime(c("2010-01-01","2019-01-01"), format = "%Y-%m-%d")) 
+
+
+df%>%
+  select(datamedicao, Volume_perc)%>% 
+  filter(Volume_perc!= 0)%>%
+  ggplot(aes(x=datamedicao, y=Volume_perc)) + 
+  #geom_point()+
+  geom_line(color='steelblue', size=3, alpha=0.4)+
+  scale_x_datetime(limits =lims)+
+  transition_reveal(datamedicao, lims)+
+  labs(title = "Castanhão", subtitle = "{frame_along}")
