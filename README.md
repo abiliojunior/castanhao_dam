a3b9f92f1d701a7bf59af2a194d71a99398d242f
abiliojunior <abiliojunior.mat@gmail.com>
2019-04-18 23:00:11 -0300

    added comments

---
+++ README.md
@@ -0,0 +1,4 @@
+#Analise do Acude Castnhão
+
+Os dados foram extraidos do portal Hídrico do estado.
+http://www.hidro.ce.gov.br/
--- run_analyses.R
+++ run_analyses.R
@@ -1,44 +1,44 @@
+#Load libraries
 library(jsonlite)
 library(tidyverse)
 library(lubridate)
 library(gganimate)
 
 
-
+#Load data
 json_file <- "./Data/raw/castanhao01.json"
 
 json_data <- fromJSON(json_file, simplifyVector = FALSE)
 
 json_data_frame <- data.frame(matrix(unlist(json_data), nrow=length(json_data), byrow=T))
 
-names(json_data_frame)<-c("datamedicao", "timestamp","cota","volume","Volume_perc")
-
-as.numeric(str_replace(json_data_frame$cota, ",", "."))-> json_data_frame$cota
-as.numeric(str_replace(json_data_frame$volume, ",", "."))-> json_data_frame$volume
-as.numeric(str_replace(json_data_frame$Volume_perc, ",", "."))-> json_data_frame$Volume_perc
+###################################Data transformation#######################################
+#Remove Column
 json_data_frame<-json_data_frame[,-2]
 
+#Rename variable
+names(json_data_frame)<-c("data_medicao","cota","volume","Volume_perc")
 
-json_data_frame$datamedicao<-as.POSIXct(json_data_frame$datamedicao, format="%Y-%m-%d")
+#Replace (,) and (.)
+json_data_frame$cota<-as.numeric(str_replace(json_data_frame$cota, ",", "."))
+json_data_frame$volume<-as.numeric(str_replace(json_data_frame$volume, ",", "."))
+json_data_frame$Volume_perc<-as.numeric(str_replace(json_data_frame$Volume_perc, ",", "."))
+
+json_data_frame$data_medicao<-as.POSIXct(json_data_frame$data_medicao, format="%Y-%m-%d")
 json_data_frame <- na.omit(json_data_frame)
 
-#######################################################################################
+########################################Analyses###############################################
 
 df<-json_data_frame
 
-#tranformando em date time posic
-
-
-
+#Setting limits for X Axis
 lims <- as.POSIXct(strptime(c("2010-01-01","2019-01-01"), format = "%Y-%m-%d")) 
 
-
 df%>%
-  select(datamedicao, Volume_perc)%>% 
+  select(data_medicao, Volume_perc)%>% 
   filter(Volume_perc!= 0)%>%
-  ggplot(aes(x=datamedicao, y=Volume_perc)) + 
-  #geom_point()+
+  ggplot(aes(x=data_medicao, y=Volume_perc)) + 
   geom_line(color='steelblue', size=3, alpha=0.4)+
   scale_x_datetime(limits =lims)+
-  transition_reveal(datamedicao, lims)+
+  transition_reveal(data_medicao, lims)+
   labs(title = "Castanhão", subtitle = "{frame_along}")
